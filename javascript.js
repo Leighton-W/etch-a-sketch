1dd8a7bce5c89d115335a16d3e936c5d387db320
Leighton Welby <lwelby27@gmail.com>
2025-06-04 18:07:26 +0100

    Added a fixed size
    made the container div a fixed height and width

--- javascript.js
+++ javascript.js
@@ -5,7 +5,7 @@ let a = 4
 for (let i = 0; i < a * a; i++){
     const container = document.querySelector("#container");
     const gridBox = document.createElement("div");
-    gridBox.textContent = "This is a box";
+    gridBox.textContent = "Box";
     gridBox.classList.add("grid-box");
     container.appendChild(gridBox);
 }
