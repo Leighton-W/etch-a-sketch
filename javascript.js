a5fa26537ae9fedd460751c18e74f6b47878d33d
Leighton Welby <lwelby27@gmail.com>
2025-06-04 17:35:21 +0100

    Updated JavaScript
    updated the javascript so that the created div would be added to the parent div container

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 
 
-let a = 2
+let a = 0
 
 for (let i = 0; i < a * a; i++){
     const gridBox = document.createElement("div");
@@ -8,3 +8,9 @@ for (let i = 0; i < a * a; i++){
     gridBox.classList.add("grid-box");
     document.body.appendChild(gridBox);
 }
+
+const container = document.querySelector("#container");
+const gridBox = document.createElement("div");
+gridBox.textContent = "This is a box";
+gridBox.classList.add("grid-box");
+container.appendChild(gridBox);
