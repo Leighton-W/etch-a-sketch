40e2f6227fb1063d810170ac5366545261d1af4f
Leighton Welby <lwelby27@gmail.com>
2025-06-04 17:37:49 +0100

    Added Flex-Wrap
    added flex-wrap to the flexbox so that the divs wrap

--- javascript.js
+++ javascript.js
@@ -1,16 +1,12 @@
 
 
-let a = 0
+let a = 4
 
 for (let i = 0; i < a * a; i++){
+    const container = document.querySelector("#container");
     const gridBox = document.createElement("div");
     gridBox.textContent = "This is a box";
     gridBox.classList.add("grid-box");
-    document.body.appendChild(gridBox);
+    container.appendChild(gridBox);
 }
 
-const container = document.querySelector("#container");
-const gridBox = document.createElement("div");
-gridBox.textContent = "This is a box";
-gridBox.classList.add("grid-box");
-container.appendChild(gridBox);
