9d4f254795578e6bf10e2ee7b3bf89affa6e676f
Leighton Welby <lwelby27@gmail.com>
2025-06-04 17:22:42 +0100

    Added loop
    created a loop to create div containers

--- javascript.js
+++ javascript.js
@@ -1,4 +1,11 @@
-const gridBox = document.createElement("div");
-gridBox.textContent = "This is a box";
-gridBox.setAttribute("style", "color: blue");
-document.body.appendChild(gridBox);
+
+
+let a = 4
+
+for (let i = 0; i < a * a; i++){
+    const gridBox = document.createElement("div");
+    gridBox.textContent = "This is a box";
+    gridBox.setAttribute("style", "color: blue", "border: dotted");
+    document.body.appendChild(gridBox);
+
+}
