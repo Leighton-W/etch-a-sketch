d338bd7a927f0ee1057a55e8a066e462734f6024
Leighton Welby <lwelby27@gmail.com>
2025-06-04 17:25:15 +0100

    Linked the JavaScript to CSS
    linked to div container created by javascript to css

--- javascript.js
+++ javascript.js
@@ -1,11 +1,13 @@
+const gridBox = document.createElement("div");
+gridBox.textContent = "This is a box";
+gridBox.classList.add("grid-box");
+document.body.appendChild(gridBox);
 
-
-let a = 4
+let a = 0
 
 for (let i = 0; i < a * a; i++){
     const gridBox = document.createElement("div");
     gridBox.textContent = "This is a box";
     gridBox.setAttribute("style", "color: blue", "border: dotted");
     document.body.appendChild(gridBox);
-
 }
