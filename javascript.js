76d503cfd4953e5c0288f0f18df41b698e7c657a
Leighton Welby <lwelby27@gmail.com>
2025-06-05 18:03:44 +0100

    Removed Some Code
    removed some of the code from JS and CSS that wasn't needed

--- javascript.js
+++ javascript.js
@@ -6,7 +6,6 @@ let gridBoxDimension = 480 / a;
 for (let i = 0; i < a * a; i++){
     const container = document.querySelector("#container");
     const gridBox = document.createElement("div");
-    gridBox.textContent = "Box";
     gridBox.classList.add("grid-box");
     gridBox.style.width = `${gridBoxDimension}px`;
     gridBox.style.height = `${gridBoxDimension}px`;
