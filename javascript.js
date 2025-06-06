72b2777527332cf651a7f44a570bbcaa6d347449
Leighton Welby <lwelby27@gmail.com>
2025-06-06 10:49:24 +0100

    Created Loop Function
    added loop to a new function

--- javascript.js
+++ javascript.js
@@ -4,23 +4,28 @@ const resetButton = document.createElement("button");
 resetButton.textContent = "Reset"
 button.appendChild(resetButton);
 
-resetButton.addEventListener("click", () => {
-    alert("Button Pressed");
-})
-
-
 
 let a = 16;
 let gridBoxDimension = 480 / a;
 
-for (let i = 0; i < a * a; i++){
-    const container = document.querySelector("#container");
-    const gridBox = document.createElement("div");
-    gridBox.classList.add("grid-box");
-    gridBox.style.width = `${gridBoxDimension}px`;
-    gridBox.style.height = `${gridBoxDimension}px`;
-    container.appendChild(gridBox);
-    gridBox.addEventListener("mouseover", () => {
-        gridBox.style.background = "black";
-    })
+function gridSettings(a){
+    for (let i = 0; i < a * a; i++){
+        const container = document.querySelector("#container");
+        const gridBox = document.createElement("div");
+        gridBox.classList.add("grid-box");
+        gridBox.style.width = `${gridBoxDimension}px`;
+        gridBox.style.height = `${gridBoxDimension}px`;
+        container.appendChild(gridBox);
+        gridBox.addEventListener("mouseover", () => {
+            gridBox.style.background = "black";
+        })
+    }
 }
+
+gridSettings(16);
+
+
+resetButton.addEventListener("click", () => {
+    let b = prompt("Select a size");
+    gridSettings(b);
+})
